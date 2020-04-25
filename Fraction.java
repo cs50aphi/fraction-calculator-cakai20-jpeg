b6b53611b0d447955e04bdf8fbfe2f20e3a49a10
cakai20-jpeg <cakai20@punahou.edu>
2020-04-25 01:56:10 +0000

    set base for fraction class and base for driver

---
+++ Fraction.java
@@ -0,0 +1,31 @@
+public class Fraction
+{
+    private int denominator;
+    
+    private int neumerator;
+    
+   public void fraction(int, int)
+   {
+       
+   }
+   
+   public int add()
+   {
+       
+   }
+   
+   public int subtract()
+   {
+       
+   }
+   
+   public int multiply()
+   {
+       
+   }
+   
+   public int divide()
+   {
+       
+   }
+}
