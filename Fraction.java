bdfe8ef19ee6a89f4117f22c5d690da529b7d7b6
cakai20-jpeg <cakai20@punahou.edu>
2020-04-26 00:38:51 +0000

    I know what I want to do with this, but i dont know how to get the methods to use a seperate constructor as input and use that to call methods for the other constructor

--- Fraction.java
+++ Fraction.java
@@ -2,31 +2,53 @@ public class Fraction
 {
     private int denominator;
     
-    private int neumerator;
+    private int numerator;
     
-   public int fraction(int x, int y)
+   public String fraction(int n, int d)
    {
-      
+      denominator = n;
+      numerator = d;
+      return neumerator + "/" + denominator;
    }
    
-   public int add()
+   public int getNum()
    {
-       
+       return nurmerator;
    }
+   
+   publinc int getDen()
+   {
+       return denominator;
+   }
+   
+   public String add()
+   {
+       newNum  = f1.getNum() * getDen() + getNum() + f1.getDen;
+       newDen = f1.getDen() * getDen();
+       return newNum + "/" + newDen;
+   }
+     
+       
    
    public int subtract()
    {
-       
+      newNum  = f1.getNum() * getDen() - getNum() + f1.getDen;
+      newDen = f1.getDen() * getDen();
+      return newNum + "/" + newDen;
+      
    }
    
    public String multiply()
    {
-       
+       newNum  = f1.getNum() * getNum();
+       newDen = f1.getDen() * getDen();
+       return newNum + "/" + newDen;
    }
    
-   public String divide( )
-   {
-      
-      
-   }
+    public String divide( )
+    {
+       newNum  = f1.getNum() * getDen();
+       newDen = f1.getDen() * getNum();
+       return newNum + "/" + newDen; 
+    }
 }
